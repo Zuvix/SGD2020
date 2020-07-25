d6d65ebd3a30b0a8a5cbc543a356f959544a0ddb
Lampask <adamgramblicka@hotmail.com>
2020-07-25 12:10:10 +0200

    Initial feature commit

---
+++ SGD/Assets/Scripts/Utils/Extenstions.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+namespace Utils
+{
+    public static class Extensions {
+        public static Vector3 ToLevelCords(this Vector3 vector)
+        {
+            vector = new Vector3(Mathf.Ceil(vector.x) + (vector.x < 0 ? 0.5f : -0.5f), 0, Mathf.Ceil(vector.z) + (vector.z < 0 ? 0.5f : -0.5f));
+            return vector;
+        }
+    }
+}

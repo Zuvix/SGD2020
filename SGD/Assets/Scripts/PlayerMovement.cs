77295f4fadf6d44e7da5165bde1711e582f6d72b
elratondesusi <37693098+elratondesusi@users.noreply.github.com>
2020-07-29 18:24:34 +0200

    New following camera for main character
    todo:
    - doublejump

--- SGD/Assets/Scripts/PlayerMovement.cs
+++ SGD/Assets/Scripts/PlayerMovement.cs
@@ -7,80 +7,55 @@ using UnityEngine.Assertions.Must;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 6.0F;
-    public float jumpSpeed = 8.0F;
-    public float gravity = 6F;
-    public float rotateSpeed = 1.0F;
-    private float verticalSpeed = 0;
-    public CharacterController characterController;
+    public Transform cam;
+    public CharacterController controller;
 
-    private int currentJump = 0;
-    private const int MAX_JUMP = 2;
+    public float speed = 6f;
+    public float gravity = -12.81f;
 
-    // Use this for initialization
-    void Start()
-    {
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
 
-    }
+    public float jumpHeigh = 3f;
+    Vector3 velocity;
+    bool isGrounded;
+    public Transform groundCheck;
+    public float groundDistance = 0.2f;
+    public LayerMask groundMask;
+
 
     // Update is called once per frame
     void Update()
     {
-        Move();
-    }
-    void Move()
-    {
-        float horizontalMove = Input.GetAxis("Horizontal");
-        float verticalMove = Input.GetAxis("Vertical");
-
-        if (characterController.isGrounded)
-            verticalSpeed = 0;
-        else
-            verticalSpeed -= gravity * Time.deltaTime;
-
-        Vector3 gravityMove = new Vector3(0, verticalSpeed, 0);
-        Vector3 move = transform.forward * verticalMove + transform.right * horizontalMove;
-
-        //jump todo, doublejump todo
-        if (characterController.isGrounded && Input.GetButton("Jump"))
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0)
         {
-            print(currentJump + " prva");
-            currentJump = 0;
-            move.y = jumpSpeed;
-            //move.y += gravity * Time.deltaTime;
-            currentJump++;
-        }
-        else if (!characterController.isGrounded && currentJump < MAX_JUMP && Input.GetButton("Jump"))
-        {
-            print(currentJump + " druhaaaaaa");
-            move.y = jumpSpeed;
-            currentJump++;
+            velocity.y = -2f;
         }
 
-        /*        if(0 < horizontalMove) { 
-                    //horizontalMove -= 2f;
-                    transform.Rotate(0, horizontalMove, 0);
-                }
-                else if (0 > horizontalMove)
-                {
-                    //horizontalMove += 2f;
-                    transform.Rotate(0, horizontalMove, 0);
-                }*/
+        float hor = Input.GetAxisRaw("Horizontal");
+        float ver = Input.GetAxisRaw("Vertical");
+        Vector3 dir = new Vector3(hor * 0.3f, 0f, ver).normalized;
+        /*if (Input.GetButton("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeigh * -2f * gravity);
+        }*/
+        transform.Translate(dir, Space.Self);
 
-        //move.y += gravity * Time.deltaTime;
-        transform.Rotate(0, horizontalMove, 0);
-        characterController.Move(speed * Time.deltaTime * move + gravityMove * Time.deltaTime);
-        transform.Rotate(0, horizontalMove, 0);
+        if (dir.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+                     controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+        if (Input.GetButton("Jump"))
+        {
+            print("jump");
+            velocity.y = jumpHeigh; // Mathf.Sqrt(jumpHeigh * -2f * gravity);
+        }
+        velocity.y -= gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
-
-
-
-
-
-
-
-
-
-
-

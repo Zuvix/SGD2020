150c662281b605dd56d239220d829a99f6036b5f
sweil9 <samuelkubala@gmail.com>
2020-07-31 12:13:29 +0200

    Abstract Enemy class

---
+++ SGD/Assets/Platforming/Enemies/Enemy.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour
+{
+    public abstract void Kill();
+    public abstract void Die();
+    public abstract void Activate();
+}

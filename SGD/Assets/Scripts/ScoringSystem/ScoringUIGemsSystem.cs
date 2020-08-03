1cca00465c5a5c2c837d360e891d7f2ee1c51d1e
elratondesusi <37693098+elratondesusi@users.noreply.github.com>
2020-08-03 13:02:35 +0200

    Scoring UI for gems, collecting gems

---
+++ SGD/Assets/Scripts/ScoringSystem/ScoringUIGemsSystem.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoringUIGemsSystem : MonoBehaviour
+{
+    public GameObject scoreText;
+    private void Update()
+    {
+        scoreText.GetComponent<Text>().text = "GEM SCORE: " + ScoringGemsSystem.gemsScore;            
+    }
+}

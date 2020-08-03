b17ca2ceb5d94227104a2b595c5521a527fe1da7
elratondesusi <37693098+elratondesusi@users.noreply.github.com>
2020-08-03 17:59:45 +0200

    lava
     done:
    -camera following player
    todo:
    -jump

--- SGD/Assets/Scripts/PlayerMovement.cs
+++ SGD/Assets/Scripts/PlayerMovement.cs
@@ -1,47 +1,33 @@
 ﻿using UnityEngine;
+using UnityEngine.Rendering;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Transform cam;
     public CharacterController controller;
+
     public Animator animator;
 
-    public float speed = 4f;
-    public float gravity = 6f;
+    public float speed = 7;
+    public float _rotationSpeed = 90;
+    private Vector3 rotation;
 
-    public float turnSmoothTime = 0.1f;
-    float turnSmoothVelocity;
-
+    public float gravity = -8f;
     public float jumpHeigh = 3f;
     private bool doubleJump = true;
-    Vector3 velocity;
+    Vector3 move;
     bool isGrounded;
     public Transform groundCheck;
     public float groundDistance = 0.2f;
     public LayerMask groundMask;
-
-
-    // Update is called once per frame
-    void Update()
+    public void Update()
     {
+        Vector3 jump = new Vector3(0.0f, 2.0f, 0.0f);
+        move = new Vector3(0, 0, Input.GetAxisRaw("Vertical") * Time.deltaTime);
+
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-        if (isGrounded && velocity.y < 0)
+        if (isGrounded && move.y < 0)
         {
-            velocity.y = -2f;
-        }
-
-        float hor = Input.GetAxisRaw("Horizontal");
-        float ver = Input.GetAxisRaw("Vertical");
-        Vector3 dir = new Vector3(hor * 0.3f, 0f, ver).normalized;
-        transform.Translate(dir, Space.Self);
-
-        if (dir.magnitude >= 0.1f)
-        {
-            float targetAngle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+            move.y = -2f;
         }
         if (isGrounded)
         {
@@ -50,10 +36,9 @@ public class PlayerMovement : MonoBehaviour
             animator.SetInteger("condition", 0);
             print("NOjump");
         }
-
-            if ((isGrounded && (Input.GetAxisRaw("Vertical") != 0 || Input.GetAxisRaw("Horizontal") != 0)))
+        if ((isGrounded && (Input.GetAxisRaw("Vertical") != 0 || Input.GetAxisRaw("Horizontal") != 0)))
         {
-            doubleJump = true; 
+            doubleJump = true;
             animator.SetInteger("jump", 0);
             animator.SetInteger("condition", 1);
             print("walking");
@@ -64,17 +49,16 @@ public class PlayerMovement : MonoBehaviour
             animator.SetInteger("condition", 0);
             print("standing");
         }
-
-        if (Input.GetButtonDown("Jump") )
+        if (Input.GetButtonDown("Jump"))
         {
             if (isGrounded)
             {
-              //  print(Input.GetAxisRaw("Vertical") + " --- " + Input.GetAxisRaw("Horizontal"));
-             //   if (Input.GetAxisRaw("Vertical") != 0 || Input.GetAxisRaw("Horizontal") != 0) { 
-              //      print("stlacena sipka");
-              //      velocity.y = jumpHeigh * 0.5f;
+                //  print(Input.GetAxisRaw("Vertical") + " --- " + Input.GetAxisRaw("Horizontal"));
+                //   if (Input.GetAxisRaw("Vertical") != 0 || Input.GetAxisRaw("Horizontal") != 0) { 
+                //      print("stlacena sipka");
+                //      velocity.y = jumpHeigh * 0.5f;
                 //} else { 
-                velocity.y = jumpHeigh;
+                move.y = jumpHeigh;
                 animator.SetInteger("jump", 1);
                 animator.SetInteger("condition", 3);
                 print("jump");
@@ -83,13 +67,20 @@ public class PlayerMovement : MonoBehaviour
             }
             else if (doubleJump)
             {
-                velocity.y = jumpHeigh;
+                move.y = jumpHeigh;
                 doubleJump = false;
                 //print("2. jump");
             }
         }
-  
-        velocity.y -= gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
+
+        this.rotation = new Vector3(0, Input.GetAxisRaw("Horizontal") * _rotationSpeed * Time.deltaTime, 0);
+
+        move = this.transform.TransformDirection(move);
+        move.y += gravity * Time.deltaTime;
+
+        controller.Move(move * speed);
+       // controller.Move(move * _speed);
+        this.transform.Rotate(this.rotation);
+
     }
 }

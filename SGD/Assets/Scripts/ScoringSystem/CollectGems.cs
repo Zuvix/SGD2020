7d665275a9e63c25beaf8d77eb19fb96ddd90230
elratondesusi <37693098+elratondesusi@users.noreply.github.com>
2020-08-11 20:54:50 +0200

    Portal effect added

--- SGD/Assets/Scripts/ScoringSystem/CollectGems.cs
+++ SGD/Assets/Scripts/ScoringSystem/CollectGems.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class CollectGems : MonoBehaviour
 {
-
+    public PortalOpen portal;
     // Update is called once per frame
     void OnTriggerEnter(Collider other)
     {
         ScoringGemsSystem.gemsScore += 1;
-        Destroy(transform.parent.gameObject);        
+        Destroy(transform.parent.gameObject);
+        if (ScoringGemsSystem.gemsScore == ScoringGemsSystem.totalScore)
+        {
+            portal.open();
+        }
     }
 }

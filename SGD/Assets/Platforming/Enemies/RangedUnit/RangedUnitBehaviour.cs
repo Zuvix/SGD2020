7d19c40ee8c56011c88d9e5e4445c7c8d53f5f07
elratondesusi <37693098+elratondesusi@users.noreply.github.com>
2020-08-11 10:40:05 +0200

    Improved

---
+++ SGD/Assets/Platforming/Enemies/RangedUnit/RangedUnitBehaviour.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+
+public class RangedUnitBehaviour : Enemy
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Activate();
+    }
+    public override void Activate()
+    {
+        //StartCoroutine("BrainScope");
+        Debug.Log("Ranged Unit Activated");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override void Kill()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Die()
+    {
+        throw new System.NotImplementedException();
+    }
+    IEnumerator BrainScope()
+    {
+        StartCoroutine("AttacLogic");
+        print("sem pridem");
+        yield return new WaitForFixedUpdate();
+    }
+    IEnumerator AttacLogic()
+    {
+        while (true)
+        {
+        
+        }
+    }
+}

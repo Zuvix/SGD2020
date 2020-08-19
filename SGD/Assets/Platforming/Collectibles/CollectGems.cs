bd60487028b3a43f7023c3372c64bbe5dc50f820
sweil9 <samuelkubala@gmail.com>
2020-08-19 20:36:41 +0200

    Gem Upscale effect

--- SGD/Assets/Platforming/Collectibles/CollectGems.cs
+++ SGD/Assets/Platforming/Collectibles/CollectGems.cs
@@ -10,11 +10,15 @@ public class CollectGems : MonoBehaviour
     private bool hit;
     Renderer m;
     Collider c;
+    Vector3 baseScale;
+    public float upscaleSpeed = 0.002f;
     private void Awake()
     {
         m = GetComponent<Renderer>();
         c = GetComponent<Collider>();
         l = GetComponent<Light>();
+        baseScale = transform.localScale;
+        transform.localScale = Vector3.zero;
         StartCoroutine(WaitAndAppear());
     }
 
@@ -37,7 +41,25 @@ public class CollectGems : MonoBehaviour
     {
         c.enabled = false;
         l.enabled = false;
-        yield return new WaitForSeconds(0.65f);
+        while (transform.localScale.x<baseScale.x && transform.localScale.y < baseScale.y && transform.localScale.z < baseScale.z)
+        {
+            Vector3 modif = new Vector3(0,0,0);
+            if (transform.localScale.x < baseScale.x)
+            {
+                modif.x += upscaleSpeed;
+            }
+            if (transform.localScale.y < baseScale.y)
+            {
+                modif.y += upscaleSpeed;
+            }
+            if (transform.localScale.z < baseScale.z)
+            {
+                modif.z += upscaleSpeed;
+            }
+            transform.localScale += modif;
+            yield return new WaitForFixedUpdate();
+        }
+        //yield return new WaitForSeconds(0.65f);
         c.enabled = true;
         l.enabled = true;
     }

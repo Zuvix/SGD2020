fd0a504160326383fb61fa154ede93600dd1b0ff
sweil9 <samuelkubala@gmail.com>
2020-08-21 20:23:22 +0200

    Update Projectile.cs

--- SGD/Assets/Platforming/Enemies/RangedUnit/Projectile.cs
+++ SGD/Assets/Platforming/Enemies/RangedUnit/Projectile.cs
@@ -17,8 +17,16 @@ public class Projectile : MonoBehaviour
     public float upscaleSpeed = 0.001f;
     public float downscaleSpeed = 0.0005f;
     public AudioSource PoofSound;
+    public AudioSource baseballHit;
     Vector3 launchDirection;
+    bool ponged = false;
     bool popped = false;
+
+    //For MEteor
+    public string type="frost";
+    public GameObject shadow;
+    Vector3 shadowBaseScale;
+    public AudioSource meteorSound;
     private void Awake()
     {
         m = GetComponent<Renderer>();
@@ -26,6 +34,11 @@ public class Projectile : MonoBehaviour
         baseScale = transform.localScale;
         transform.localScale = Vector3.zero;
         rb = GetComponent<Rigidbody>();
+        if (shadow != null)
+        {
+            shadowBaseScale = shadow.transform.localScale;
+            shadow.transform.localScale = Vector3.zero;
+        }
     }
 
     // Start is called before the first frame update
@@ -34,6 +47,7 @@ public class Projectile : MonoBehaviour
         player = GameObject.FindGameObjectWithTag("Player").transform;
         target = new Vector3(player.position.x, player.position.y + 0.24f, player.position.z);
         StartCoroutine(SpawnPojectile());
+
     }
 
     public IEnumerator SpawnPojectile()
@@ -55,11 +69,24 @@ public class Projectile : MonoBehaviour
                 modif.z += upscaleSpeed;
             }
             transform.localScale += modif;
+            if (shadow != null)
+            {
+                shadow.transform.localScale += modif/300;
+            }
             yield return new WaitForFixedUpdate();
         }
         //yield return new WaitForSeconds(0.65f);
         launchDirection = new Vector3(player.position.x, player.position.y + 0.24f, player.position.z)*10000;
         c.enabled = true;
+        if (type.Equals("fire"))
+        {
+            FallDown();
+            
+        }
+        if (meteorSound != null)
+        {
+            meteorSound.Play();
+        }
     }
     public IEnumerator FlyTowardsPlayer()
     {
@@ -90,24 +117,25 @@ public class Projectile : MonoBehaviour
 
         }
         transform.parent = null;
-        while (player != null && !popped && timeFlying < 4.5f)
+        while (player != null && !popped && timeFlying < 4.5f &&!ponged )
         {
             timeFlying += Time.deltaTime;
             if (type.Equals("up"))
             {
                 transform.Rotate(Vector3.up * 5f);
-                transform.position = Vector3.MoveTowards(transform.position, player.position + new Vector3(0f, 0.45f, 0f), speed * 1.25f);
-                transform.Translate(target * speed * 0.45f);
+                transform.position = Vector3.MoveTowards(transform.position, player.position + new Vector3(0f, 0.45f, 0f), speed * 1.4f);
+                transform.Translate(target * speed * 0.4825f);
             }
             if (type.Equals("right"))
             {
-                transform.Rotate(Vector3.right * 1.05f);
-                transform.position = Vector3.MoveTowards(transform.position, player.position + new Vector3(0f, 0.55f, 0f), speed * 1f);
-                transform.Translate(target * speed * 0.85f);
+                transform.Rotate(Vector3.right * 1f);
+                transform.position = Vector3.MoveTowards(transform.position, player.position + new Vector3(0f, 0.55f, 0f), speed * 1.2f);
+                transform.Translate(target * speed * 0.9f);
             }
             yield return new WaitForFixedUpdate();
         }
-        Pop();
+        if(timeFlying>=4.5f)
+            Pop();
     }
     IEnumerator DownScale()
     {
@@ -131,6 +159,23 @@ public class Projectile : MonoBehaviour
         }
         yield return new WaitForFixedUpdate();
     }
+    IEnumerator Homerun()
+    {
+        owner = player.gameObject;
+        Vector3 direction = player.transform.forward;
+        transform.rotation = Quaternion.LookRotation(transform.position-player.forward);
+        ponged = true;
+        baseballHit.Play();
+        while (!popped)
+        {
+            transform.Translate(direction*speed*2.4f);
+            yield return new WaitForFixedUpdate();
+        }
+    }
+    public void FallDown()
+    {
+        rb.isKinematic = false;
+    }
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("Ground")|| other.gameObject.CompareTag("LivingGround")|| other.gameObject.CompareTag("Wall"))
@@ -140,6 +185,24 @@ public class Projectile : MonoBehaviour
         if (other.gameObject.CompareTag("Enemy") &&!other.gameObject.Equals(owner))
         {
             other.GetComponent<Enemy>().Die();
+        }
+        if (other.gameObject.CompareTag("Player"))
+        {
+            PlayerBehaviour pb = other.gameObject.GetComponent<PlayerBehaviour>();
+            if (!pb.isAttacking || type.Equals("fire"))
+            {
+                pb.Die();
+                Pop();
+            }
+            else
+            {
+                StartCoroutine(Homerun());
+            }
+
+        }
+        if (other.gameObject.CompareTag("Bck"))
+        {
+            Pop();
         }
     }
 
@@ -154,6 +217,10 @@ public class Projectile : MonoBehaviour
             GameObject g = Instantiate(poofEffect);
             g.transform.position = transform.position;
             Destroy(this.gameObject, 0.4f);
+            if (type.Equals("fire"))
+            {
+                Destroy(transform.parent.gameObject, 0.4f);
+            }
             popped = true;
         }
     }

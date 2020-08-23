c412969c5234b9f194dc2e91c0579a5ff7da684f
sweil9 <samuelkubala@gmail.com>
2020-08-23 23:07:10 +0200

    Music

---
+++ SGD/Assets/Platforming/VolumeFIxer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeFIxer : MonoBehaviour
+{
+    public float setVolume = 0.6f;
+    private void Awake()
+    {
+        AudioListener.volume = setVolume;
+    }
+}
